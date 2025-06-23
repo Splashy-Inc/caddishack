63936aa6d8739014f74c202e9291bda69639cb14
avelitj12 <avelitj12@gmail.com>
2025-06-22 22:20:16 -0400

    Finish bead pile
    Use for both draw and discard piles to show drawing and adding beads to piles

--- SCRIPTS/bead_pile.gd
+++ SCRIPTS/bead_pile.gd
@@ -5,7 +5,7 @@ class_name BeadPile
 @export var info : BeadPileInfo
 @export var bead_scene : PackedScene
 
-@onready var bead_container: Node2D = $Beads
+@onready var bead_container: Node2D = $Beads/Container
 @onready var bead_container_shape: CollisionShape2D = $Beads/CollisionShape2D
 
 func _ready() -> void:
@@ -28,11 +28,15 @@ func add_bead(new_bead: Bead):
 	if new_bead in bead_container.get_children():
 		print("Bead already in pile!", new_bead, " ", self)
 		return
-	bead_container.add_child(new_bead)
+	if new_bead.get_parent():
+		new_bead.reparent(bead_container)
+	else:
+		bead_container.add_child(new_bead)
 	if bead_container_shape.shape is CircleShape2D:
 		new_bead.position = Vector2(1000, 1000)
+		new_bead.rotation = randf_range(0.0, PI*2)
 		while new_bead.global_position.distance_to(bead_container.global_position) > bead_container_shape.shape.radius:
-			new_bead.position = Vector2(randi_range(0, bead_container_shape.shape.radius), randi_range(0, bead_container_shape.shape.radius))
+			new_bead.position = Vector2(randi_range(-bead_container_shape.shape.radius, bead_container_shape.shape.radius), randi_range(-bead_container_shape.shape.radius, bead_container_shape.shape.radius))
 
 func draw_bead() -> Bead:
 	var bead = bead_container.get_children().pick_random()
