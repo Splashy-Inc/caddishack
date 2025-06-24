f1db8588b35f3f9b44b95b330d9940b2446d4485
Tristan <avelitj12@gmail.com>
2025-06-24 18:12:23 -0400

    Randomly spawn placeholder materials

--- RESOURCES/run_info.gd
+++ RESOURCES/run_info.gd
@@ -3,6 +3,6 @@ extends Resource
 class_name RunInfo
 
 @export var money := 0
-@export var terrarium := TerrariumInfo
+@export var terrarium : TerrariumInfo
 @export var bead_pile : BeadArrayInfo
 @export var bracelets : Array[BraceletInfo]
--- SCRIPTS/TERRARIUM/terrarium_station.gd
+++ SCRIPTS/TERRARIUM/terrarium_station.gd
@@ -1,3 +1,19 @@
 extends Station
 
 class_name TerrariumStation
+
+@export var info : TerrariumInfo
+@export var material_scene : PackedScene
+
+@onready var container_layer: TerrariumBackground = $PlayingField/ContainerLayer
+@onready var playing_field: Node2D = $PlayingField
+@onready var materials_container: Node2D = $PlayingField/Materials
+
+func _station_ready():
+	for color in SandMaterialInfo.SandColor:
+		info.materials.append(MaterialInfo)
+	
+	for material in info.materials:
+		var new_material = material_scene.instantiate() as BeadMaterial
+		materials_container.add_child(new_material)
+		new_material.global_position = container_layer.get_spawnable_cell_center()
