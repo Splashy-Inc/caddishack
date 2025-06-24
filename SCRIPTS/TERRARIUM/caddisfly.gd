88572a57858560409d24f3ed432dc7883ef87f56
avelitj12 <avelitj12@gmail.com>
2025-06-24 18:43:36 -0400

    Add in animation control
    Based on moving/idle

--- SCRIPTS/TERRARIUM/caddisfly.gd
+++ SCRIPTS/TERRARIUM/caddisfly.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = -400.0
 var speed_mod := 1.0
 var direction : Vector2
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _physics_process(delta: float) -> void:	
 	var new_direction = Vector2.ZERO
 	if Globals.is_mobile and Globals.joystick:
@@ -17,11 +19,11 @@ func _physics_process(delta: float) -> void:
 	if new_direction and new_direction != Vector2.ZERO:
 		speed_mod = 1.0
 		direction = direction.lerp(new_direction.normalized(), .1)
-		#animation_player.play("move")
+		animation_player.play("move")
 		rotation = -direction.angle_to(Vector2.UP)
 	else:
 		speed_mod = 0.0
-		#animation_player.play("idle")
+		animation_player.play("idle")
 		
 	velocity = direction * SPEED * speed_mod
 		
