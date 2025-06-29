4eaadd3e2d56b55dd238f3cd85855254b8b7e757
avelitj12 <avelitj12@gmail.com>
2025-06-29 16:28:19 -0400

    Update materials in run info

--- SCRIPTS/CRAFT_STATION/run_info_menu.gd
+++ SCRIPTS/CRAFT_STATION/run_info_menu.gd
@@ -5,6 +5,7 @@ class_name RunInfoMenu
 signal exited
 
 @onready var money_value: Label = $PanelContainer/VBoxContainer/MoneyBox/HBoxContainer/Value
+@onready var material_panel: CartPanel = $PanelContainer/VBoxContainer/ThingsInfobox/MaterialsBox/BeadPile_Background/SubViewport/MaterialPanel
 @onready var bead_pile: BeadPile = $PanelContainer/VBoxContainer/ThingsInfobox/BeadPileBox/BeadPile_Background/SubViewport/BeadPile
 @onready var complete_bracelet_panel: BraceletContainer = $PanelContainer/VBoxContainer/ThingsInfobox/BraceletsBox/Bracelet_Background/SubViewport/CompleteBraceletPanel
 
@@ -19,6 +20,7 @@ func _process(delta: float) -> void:
 
 func load_run_info():
 	money_value.text = str(Globals.run_info.money)
+	material_panel.set_materials(Globals.run_info.terrarium.materials)
 	bead_pile.set_beads(Globals.run_info.bead_pile)
 	complete_bracelet_panel.fill_bracelets(Globals.run_info.bracelets)
 
