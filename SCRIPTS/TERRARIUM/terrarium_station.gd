86a6c652964fabb55f7636b85d9106a3826a522e
Tristan <avelitj12@gmail.com>
2026-03-21 18:54:59 -0400

    Cleanup

--- SCRIPTS/TERRARIUM/terrarium_station.gd
+++ SCRIPTS/TERRARIUM/terrarium_station.gd
@@ -1,12 +1,3 @@
 extends Station
 
 class_name TerrariumStation
-
-@export var material_scene : PackedScene
-
-@onready var terrarium: Terrarium = $PlayScreen/PlayingField/Terrarium
-@onready var playing_field: Node = $PlayScreen/PlayingField
-
-
-func _station_ready():
-	pass
