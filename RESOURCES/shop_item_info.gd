f66cdae006251edca5de0db5ea3d7a55badd982c
Tristan <avelitj12@gmail.com>
2026-04-06 08:22:03 -0400

    Set up example shop items

---
+++ RESOURCES/shop_item_info.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name ShopItemInfo
+
+@export var icon : Texture2D
+@export var name : String
+@export var base_cost : int
+@export var description : String
--- SCRIPTS/SHOP/shop_item.gd
+++ SCRIPTS/SHOP/shop_item.gd
@@ -2,11 +2,17 @@ extends Button
 
 class_name ShopItem
 
+@export var info : ShopItemInfo
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var icon_sprite: Sprite2D = $ItemSpace/TopHalf/IconSpace/Icon
+@onready var name_label: Label = $ItemSpace/TopHalf/TopRightSpace/Name
+@onready var cost_label: Label = $ItemSpace/TopHalf/TopRightSpace/Cost
+@onready var description_label: Label = $ItemSpace/Description
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	load_info(info)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,3 +23,10 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
 	animation_player.play("RESET")
+
+func load_info(new_info: ShopItemInfo):
+	info = new_info
+	icon_sprite.texture = info.icon
+	name_label.text = info.name
+	cost_label.text = str(info.base_cost)
+	description_label.text = info.description
