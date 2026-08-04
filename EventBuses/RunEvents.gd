c2de4f67c846173945df715a88d9f775866e6447
Tristan <avelitj12@gmail.com>
2026-08-04 08:59:19 -0400

    Set back to new run info for game export

--- EventBuses/RunEvents.gd
+++ EventBuses/RunEvents.gd
@@ -22,7 +22,7 @@ var test_run_info := preload("res://RESOURCES/test_run.tres")
 func _ready() -> void:
 	score_generated.connect(change_score)
 	round_started.connect(increment_round)
-	load_run_info(test_run_info)
+	load_run_info(new_run_info)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -68,7 +68,7 @@ func change_score(change: int):
 	set_score(run_info.score + change)
 
 func reset_run():
-	load_run_info(test_run_info.duplicate(true))
+	load_run_info(new_run_info.duplicate(true))
 
 func load_run_info(loaded_run_info: RunInfo):
 	set_round(loaded_run_info.cur_round, loaded_run_info.max_rounds)
