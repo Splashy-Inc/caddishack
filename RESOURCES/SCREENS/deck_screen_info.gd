5b60a215566f06c9f47f7e204af53dbbed9c47ef
Tristan <avelitj12@gmail.com>
2026-08-18 20:59:35 -0400

    Remove terrarium screen info
    Never used it

--- RESOURCES/SCREENS/deck_screen_info.gd
+++ RESOURCES/SCREENS/deck_screen_info.gd
@@ -4,5 +4,4 @@ class_name DeckScreenInfo
 
 @export var deck : DeckInfo
 @export var shop_info : ShopInfo
-@export var terrarium_screen_info : TerrariumScreenInfo
 @export var ability_info : ShopAbilityInfo
