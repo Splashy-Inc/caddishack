3604f2a59508a48e19c69b0690656bf6fe4e2dcc
Tristan <avelitj12@gmail.com>
2026-08-20 19:33:51 -0400

    Initial menu button sounds

--- SCRIPTS/GUI/start_button.gd
+++ SCRIPTS/GUI/start_button.gd
@@ -1,27 +1,10 @@
-extends Button
+extends UIButton
 
 @export var scene_to_start : PackedScene
-@onready var hover_sound: AudioStreamPlayer = $HoverSound
-@onready var press_sound: AudioStreamPlayer = $PressSound
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_pressed() -> void:
-	press_sound.play()
 	RunEvents.reset_run()
 	var random_terrarium = TerrariumInfo.new()
 	random_terrarium.randomize_materials()
 	RunEvents.set_current_terrarium_info(random_terrarium)
 	ScreenEvents.request_screen(ScreenEvents.Screen.TERRARIUM)
-
-
-func _on_mouse_entered() -> void:
-	hover_sound.play()
---
+++ SCRIPTS/GUI/ui_button.gd
@@ -0,0 +1,38 @@
+extends Button
+
+class_name UIButton
+
+@export var hover_sound_scene: PackedScene = preload("uid://b541wyt8ubmd7")
+@export var press_sound_scene: PackedScene = preload("uid://dsismqget8078")
+
+var hover_sound := AudioStreamPlayer.new()
+var press_sound := AudioStreamPlayer.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if hover_sound_scene:
+		hover_sound = hover_sound_scene.instantiate()
+	if press_sound_scene:
+		press_sound = press_sound_scene.instantiate()
+	add_child(hover_sound)
+	add_child(press_sound)
+	
+	pressed.connect(play_pressed_sound)
+	mouse_entered.connect(play_hover_sound)
+	_button_ready()
+
+# To be overridden by child to handle any ready event things while retaining above behavior
+func _button_ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func play_pressed_sound():
+	if is_instance_valid(press_sound):
+		press_sound.play()
+
+func play_hover_sound():
+	if is_instance_valid(hover_sound):
+		hover_sound.play()
