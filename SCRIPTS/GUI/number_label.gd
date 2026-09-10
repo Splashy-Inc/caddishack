3bfeadd22f4a5ed16c9cf66124638a20ea096163
Tristan <avelitj12@gmail.com>
2026-09-10 08:39:01 -0400

    Add shake to score/money change

--- SCRIPTS/GUI/number_label.gd
+++ SCRIPTS/GUI/number_label.gd
@@ -4,6 +4,9 @@ class_name NumberLabel
 
 signal change_complete
 
+@export var shake_strength := 5.0
+@export var shake_fade_time := 5.0
+
 @onready var change_up_sound: AudioStreamPlayer = $ChangeUpSound
 @onready var change_down_sound: AudioStreamPlayer = $ChangeDownSound
 
@@ -26,9 +29,11 @@ func _process(delta: float) -> void:
 				number = floor(lerp(number, number_target, time_to_target * delta))
 				change_down_sound.play()
 			text = str(number)
+			offset_transform_position = getRandomOffset()
 		else:
 			set_number(number_target)
 			change_complete.emit()
+			offset_transform_position = Vector2.ZERO
 
 func set_number(new_number : int, time : float = 0.0):
 	time_to_target = time
@@ -37,4 +42,6 @@ func set_number(new_number : int, time : float = 0.0):
 	else:
 		number = new_number
 		text = str(number)
-		
+
+func getRandomOffset() -> Vector2:
+	return Vector2(randf_range(-shake_strength, shake_strength), randf_range(-shake_strength, shake_strength))
